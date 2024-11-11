f5a2d3788b4c0f64a3390105621642de7b5901ee
Kardorgon <kam.wojciuk@gmail.com>
2024-11-11 17:19:05 +0100

    Add hideable menu with navigation buttons
    Related to #36
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Kardorgon/grocery-helper/issues/36?shareId=XXXX-XXXX-XXXX-XXXX).

--- grocery-helper/src/components/app-root/app-root.tsx
+++ grocery-helper/src/components/app-root/app-root.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
 
 @Component({
   tag: 'app-root',
@@ -6,9 +6,14 @@ import { Component, h } from '@stencil/core';
   // shadow: true,
 })
 export class AppRoot {
+  @State() menuOpen: boolean = false;
 
   componentWillLoad() {
     console.log('AppRoot component is about to load');
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
   }
 
   render() {
@@ -21,9 +26,38 @@ export class AppRoot {
           <ion-route url="/profile/:name" component="page-profile"></ion-route>
           <ion-route url="/shopping-list" component="shopping-list"></ion-route>
         </ion-router>
-        <ion-router-outlet>
-          <app-tabs></app-tabs>
-        </ion-router-outlet>
+        <ion-menu side="start" menuId="first" contentId="main-content" swipeGesture={true} disabled={!this.menuOpen}>
+          <ion-header>
+            <ion-toolbar>
+              <ion-title>Menu</ion-title>
+            </ion-toolbar>
+          </ion-header>
+          <ion-content>
+            <ion-list>
+              <ion-item button href="/home">
+                <ion-icon slot="start" name="home"></ion-icon>
+                <ion-label>Home</ion-label>
+              </ion-item>
+              <ion-item button href="/shopping-list">
+                <ion-icon slot="start" name="cart"></ion-icon>
+                <ion-label>Shopping List</ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-content>
+        </ion-menu>
+        <div id="main-content">
+          <ion-header>
+            <ion-toolbar>
+              <ion-buttons slot="start">
+                <ion-menu-button onClick={() => this.toggleMenu()}></ion-menu-button>
+              </ion-buttons>
+              <ion-title>Grocery Helper</ion-title>
+            </ion-toolbar>
+          </ion-header>
+          <ion-content>
+            <ion-router-outlet></ion-router-outlet>
+          </ion-content>
+        </div>
       </ion-app>
     )
   }
