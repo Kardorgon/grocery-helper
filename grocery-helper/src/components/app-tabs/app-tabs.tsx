204cd7fff181ac8d68de5f4f3f36a3aadb374039
Kardorgon <kam.wojciuk@gmail.com>
2024-11-11 17:22:47 +0100

    Fix burger menu and home page visibility
    Related to #38
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Kardorgon/grocery-helper/issues/38?shareId=XXXX-XXXX-XXXX-XXXX).

--- grocery-helper/src/components/app-tabs/app-tabs.tsx
+++ grocery-helper/src/components/app-tabs/app-tabs.tsx
@@ -18,6 +18,16 @@ export class AppTabs {
         </ion-tab>
         <ion-tab tab="tab-shopping-list">
         </ion-tab>
+        <ion-tab-bar slot="top">
+          <ion-tab-button tab="tab-home">
+            <ion-icon name="home"></ion-icon>
+            <ion-label>Home</ion-label>
+          </ion-tab-button>
+          <ion-tab-button tab="tab-shopping-list">
+            <ion-icon name="cart"></ion-icon>
+            <ion-label>Shopping List</ion-label>
+          </ion-tab-button>
+        </ion-tab-bar>
       </ion-tabs>
     );
   }
